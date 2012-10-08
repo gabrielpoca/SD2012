86ff49bcd6bdde3a1ca5a6f4a470b1489ef02428
Gabriel Poça <gabrielpoca@gmail.com>
2012-10-08 17:30:20 +0100

    added basic database classes

--- StockExchange/src/stockexchange/StockExchange.java
+++ StockExchange/src/stockexchange/StockExchange.java
@@ -1,6 +1,29 @@
 
 package stockexchange;
 
+import java.net.Socket;
+import java.util.ArrayList;
+
+class DatabaseEntry {
+    private Socket socket;
+    private int value;
+    public DatabaseEntry(Socket socket, int value) {
+	this.socket = socket;
+	this.value = value;
+    }
+    public Socket getSocket() {
+	return socket;
+    }
+    public int getValue() {
+	return value;
+    }
+}
+
+class Database {
+    private ArrayList<DatabaseEntry> sell_databe;
+    private ArrayList<DatabaseEntry> buy_database;
+}
+
 public class StockExchange {
 
     public static void main(String[] args) {
