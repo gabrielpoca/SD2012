67a7b93a646460f4d5b285a148b40eb374aa28b9
Gabriel Poça <gabrielpoca@gmail.com>
2012-10-22 16:31:04 +0100

    Now there are two threads, a reader and a writer.

--- StockExchangeClient/src/stockexchangeclient/StockExchangeClient.java
+++ StockExchangeClient/src/stockexchangeclient/StockExchangeClient.java
@@ -11,27 +11,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+class Reader extends Thread {
+    
+    Socket socket;
+    private boolean run;
+    
+    public Reader(Socket socket) {
+        this.socket = socket;
+        run = true;
+    }
+        
+    public void run() {
+        try {
+            log("Strating reader...");
+            DataInputStream input_stream = new DataInputStream(socket.getInputStream());
+            String message = "";
+            while(run) {
+                message = input_stream.readUTF();
+                log(message);
+            }
+            log("Closing reader...");
+            input_stream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Reader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void end() {
+        run = false;
+    }
+    
+    private void log(String s) {
+        System.out.println("[Reader] "+s);
+    }
+}
 
 public class StockExchangeClient {
 
-    public static void main(String args[]) throws IOException, UnknownHostException {
-	Socket cs = new Socket("localhost", 9999);
-
-	DataInputStream is = new DataInputStream(cs.getInputStream());
-	DataOutputStream os = new DataOutputStream(cs.getOutputStream());
-
+    public static void main(String args[]) throws IOException, UnknownHostException, InterruptedException {
+	Socket socket = new Socket("localhost", 9999);
+        
+        // start reader
+        Reader reader = new Reader(socket);
+        Thread thread_reader = new Thread(reader);
+        thread_reader.start();
+        // start writter
+        boolean run = true;
+	DataOutputStream output_stream = new DataOutputStream(socket.getOutputStream());
 	String current;
-	BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
-
-	os.writeUTF(sin.readLine());
-	System.out.println(is.readInt());
-	current = sin.readLine();
-	os.writeUTF(current);
-
-	is.close();
-	os.close();
+        BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));        
+        while(run) {
+            current = sin.readLine();
+            if(current.equals("exit")) {
+                run = false;
+            }
+            output_stream.writeUTF(current);
+        }
+        // stop reader
+        reader.end();
+        thread_reader.join();
+        // close sockets and streams
+	output_stream.close();
 	sin.close();
-	cs.close();
+	socket.close();
     }
 }
